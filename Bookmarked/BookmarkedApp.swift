c23999208731cd40c1ee84c15b4ab379d70daa0e
Vivien Quach <vivienquach6@gmail.com>
2023-08-22 14:16:16 -0500

    Initial Commit

---
+++ Bookmarked/BookmarkedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkedApp.swift
+//  Bookmarked
+//
+//  Created by Vivien on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
