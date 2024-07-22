0fd409f44f7c1f7a43c5c83288c69640516abae4
Vivien Quach <vivienquach6@gmail.com>
2024-07-22 15:36:55 -0500

    -Added more logic for fetching/updating user data. -User Profile View UI updated. -Created separate Registration View. -Mock Firestore Service and XCTests updated.

---
+++ Bookmarked/UserReviewWithBookView.swift
@@ -0,0 +1,18 @@
+//
+//  UserReviewWithBookView.swift
+//  Bookmarked
+//
+//  Created by Vivien on 7/9/24.
+//
+
+import SwiftUI
+
+struct UserReviewWithBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserReviewWithBookView()
+}
---
+++ Bookmarked/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  Bookmarked
+//
+//  Created by Vivien on 7/10/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
